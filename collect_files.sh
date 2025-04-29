c081a5f65f7d9d871c62edf397289f27bf41fe91
Pavel Kachalo <opavelkachalo@gmail.com>
2025-04-30 01:17:15 +0300

    arguments parsing added to collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,1 +1,36 @@
-#! /usr/bin/bash
+#!/bin/bash
+
+argc=$#
+
+if [ $argc -lt 2 ]; then
+    echo 'Usage: ./collect_files.sh <source> <dest> [--max_depth=N]' >&2
+    exit 1
+fi
+
+source=$1
+dest=$2
+
+if [ $argc -eq 3 ]; then
+    if [[ $3 =~ ^--max_depth=[0-9]+ ]]; then
+        third_arg=$3
+        max_depth=${third_arg:12}
+    else
+        echo "Error: Incorrect argument ${3}" >&2
+        echo 'Usage: ./collect_files.sh <source> <dest> [--max_depth=N]' >&2
+        exit 1
+    fi
+else
+    max_depth=1
+fi
+
+if [ ! -d $source ]; then
+    echo "Error: $source is not a directory" >&2
+    exit 1
+fi
+
+if [ ! -e $dest ]; then
+    mkdir $dest
+elif [ ! -d $dest ]; then
+    echo "Error: $dest is not a directory" >&2
+    exit 1
+fi
